6e575cad40127e233e5716fa98a571a1256769b2
Joe Carrafa <joe.carrafa@warbyparker.com>
2017-02-10 17:49:04 -0500

    Add desk-dock-server  v1.0.8

---
+++ Casks/desk-dock-server.rb
@@ -0,0 +1,10 @@
+cask 'desk-dock-server' do
+  version '1.0.8'
+  sha256 '7c5348900dff241e3d364b4fd111ff4c3b8c4ef53c58af5c9984721810b04b81'
+
+  url 'https://www.dropbox.com/s/axs75vy7ogsish7/DeskDockServer_Mac_1.0.8.zip?dl=1'
+  name 'Desk Dock'
+  homepage 'http://fdmobileinventions.blogspot.com/p/deskdock-server.html'
+
+  app 'DeskDockServer_1.0.8.app'
+end
