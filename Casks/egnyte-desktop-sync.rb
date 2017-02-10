2804d9d6c95f32f3e8efe2550b5fdabd51832b00
Joe Carrafa <joe.carrafa@warbyparker.com>
2017-02-10 17:49:04 -0500

    Add egnyte-desktop-sync  v8.5.3

---
+++ Casks/egnyte-desktop-sync.rb
@@ -0,0 +1,11 @@
+cask 'egnyte-desktop-sync' do
+  version '8.5.3'
+  sha256 'a7b34e07f8af3b19dbd19f638298bfdd82ba2b09e881a4ac768f788f3c379522'
+
+  url 'https://atlas.warby.io/rest/public/1.0/apps/downloads/plc/mac'
+  name 'EgnyteDesktopSync'
+  homepage ''
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'Egnyte Desktop Sync Installer.app'
+end
